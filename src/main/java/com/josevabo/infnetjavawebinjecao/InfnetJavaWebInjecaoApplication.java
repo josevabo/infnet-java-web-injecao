12b9e439c9170826bbe11015e8a87e341edb9a77
josevabo <joseibpn@gmail.com>
2023-10-04 19:33:00 -0300

    Inicio projeto

---
+++ src/main/java/com/josevabo/infnetjavawebinjecao/InfnetJavaWebInjecaoApplication.java
@@ -0,0 +1,13 @@
+package com.josevabo.infnetjavawebinjecao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfnetJavaWebInjecaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfnetJavaWebInjecaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/josevabo/infnetjavawebinjecao/InfnetJavaWebInjecaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josevabo.infnetjavawebinjecao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfnetJavaWebInjecaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
